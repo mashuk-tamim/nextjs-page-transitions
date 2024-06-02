794711b4679bceea3cdc230805ce84055b18b83c
mashuk-tamim <tamimmashuk328@gmail.com>
2024-06-02 18:18:36 +0600

    stair transition fixed: exits from top to bottom and enters at bottom to top

--- src/components/Layout/Stairs/anim.js
+++ src/components/Layout/Stairs/anim.js
@@ -1,58 +1,21 @@
 // bottom >>>> top
 
-// export const expand = {
-// 	initial: {
-// 		top: "0vh",
-// 	},
-// 	enter: (i) => ({
-// 		top: "100vh",
-// 		transition: {
-// 			duration: 0.4,
-// 			delay: 0.05 * i,
-// 			ease: [0.215, 0.61, 0.355, 1],
-// 		},
-// 		transitionEnd: { height: "0vh", top: "0vh" },
-// 	}),
-// 	exit: (i) => ({
-// 		height: "100vh",
-// 		transition: {
-// 			duration: 0.4,
-// 			delay: 0.05 * i,
-// 			ease: [0.215, 0.61, 0.355, 1],
-// 		},
-// 	}),
-// };
-// export const opacity = {
-// 	initial: {
-// 		opacity: 0.5,
-// 	},
-// 	enter: {
-// 		opacity: 0,
-// 	},
-// 	exit: {
-// 		opacity: 0.5,
-// 	},
-// };
-
-
-// top >>>> bottom
-
 export const expand = {
 	initial: {
-		top: 0, // Initial position at the top
+		bottom: 0, // Initial position at the bottom
 	},
 	enter: (i) => ({
-		top: "100vh", // Move downwards
+		bottom: "100vh", // 
 		transition: {
 			duration: 0.4,
 			delay: 0.05 * i,
 			ease: [0.215, 0.61, 0.355, 1],
 		},
-		transitionEnd: { height: "0", top: "0" }, // End with height 0 and position back at top
+		transitionEnd: { height: "0vh", top: "0vh" }, // End with height 0 and position back at bottom
 	}),
 	exit: (i) => ({
 		height: "100vh",
-		top: 0, // Start from top
+		bottom: "100vh", // Start from bottom
 		transition: {
 			duration: 0.4,
 			delay: 0.05 * i,
@@ -72,3 +35,42 @@ export const opacity = {
 		opacity: 0.5,
 	},
 };
+
+
+// top >>>> bottom
+
+// export const expand = {
+// 	initial: {
+// 		top: 0, // Initial position at the top
+// 	},
+// 	enter: (i) => ({
+// 		top: "100vh", // Move downwards
+// 		transition: {
+// 			duration: 0.4,
+// 			delay: 0.05 * i,
+// 			ease: [0.215, 0.61, 0.355, 1],
+// 		},
+// 		transitionEnd: { height: "0", top: "0" }, // End with height 0 and position back at top
+// 	}),
+// 	exit: (i) => ({
+// 		height: "100vh",
+// 		top: 0, // Start from top
+// 		transition: {
+// 			duration: 0.4,
+// 			delay: 0.05 * i,
+// 			ease: [0.215, 0.61, 0.355, 1],
+// 		},
+// 	}),
+// };
+
+// export const opacity = {
+// 	initial: {
+// 		opacity: 0.5,
+// 	},
+// 	enter: {
+// 		opacity: 0,
+// 	},
+// 	exit: {
+// 		opacity: 0.5,
+// 	},
+// };
